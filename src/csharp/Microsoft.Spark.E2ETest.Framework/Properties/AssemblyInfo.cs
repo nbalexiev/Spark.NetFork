9daae121e9f75afea4062c7f1986f56ab84fad4b
Nikolay Aleksiev <nialeksi@microsoft.com>
2020-06-30 21:55:13 -0700

    Add .Net Framework E2E Test.
    Clean up.
    
    Generate pdb.

---
+++ src/csharp/Microsoft.Spark.E2ETest.Framework/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Microsoft.Spark.E2ETest.Framework")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Microsoft.Spark.E2ETest.Framework")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("010edd48-c971-44e8-882d-47d9735d9403")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.12.1.0")]
+[assembly: AssemblyFileVersion("0.12.1.0")]
